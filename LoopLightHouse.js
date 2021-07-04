f6cbfa361980663d6e9b749526f95d4005242494
Saif Ali <saifali7646@gmail.com>
2021-07-04 01:30:19 +0000

    update in LoopLightHouse.js

--- LoopLightHouse.js
+++ LoopLightHouse.js
@@ -1,16 +1,12 @@
-for(let x=100; x <=200; x++){
-  
-  if (x % 3===0 && x%4===0){
-    console.log('LoopyLighthouse');
-  }
-  else if (x % 3===0){
-   console.log('Loopy');
-  }
+for (let x = 100; x <= 200; x++) {
 
-  else if (x % 4===0){
+  if (x % 3 === 0 && x % 4 === 0) {
+    console.log('LoopyLighthouse');
+  } else if (x % 3 === 0) {
+    console.log('Loopy');
+  } else if (x % 4 === 0) {
     console.log('Lighthouse');
-  }
-  else {
+  } else {
     console.log(x);
   }
 
